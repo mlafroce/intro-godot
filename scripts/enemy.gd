c05346131bcc31f1f18ce9f5c59802692df10e61
Matías Lafroce <mlafroce@gmail.com>
2024-11-24 11:32:55 -0300

    Add damage animation with shader

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,13 +1,30 @@
 extends CharacterBody2D
 
 const SPEED = 150.0
+const MAX_GLOW = 1.0
+const MIN_GLOW = 0.0
+const GLOW_STEP = 10.0
 
 @onready var player = get_node("/root/MainGame/Player")
+
 var health = 3
+var current_glow = MIN_GLOW
+
+func _ready() -> void:
+	var shader = %PlaceholderSprite.material as ShaderMaterial
+	# Instance uniforms are not implemented, workaround!
+	%PlaceholderSprite.material = shader.duplicate()
+
+func _process(delta: float) -> void:
+	var shader = %PlaceholderSprite.material as ShaderMaterial
+	shader.set_shader_parameter("EXTRA_GLOW", current_glow)
+	if current_glow > 0.0:
+		current_glow -= GLOW_STEP * delta
+		if current_glow < 0.0:
+			current_glow = 0.0		
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
-	
 	velocity = direction * SPEED
 	
 	var dir_normalized = direction.normalized()
@@ -19,6 +36,9 @@ func _physics_process(delta: float) -> void:
 
 func take_damage(dmg: int) -> void:
 	health -= dmg
+	current_glow = MAX_GLOW;
+	var shader = %PlaceholderSprite.material as ShaderMaterial
+	shader.set_shader_parameter("EXTRA_GLOW", current_glow)
 	
 	if health <= 0:
 		queue_free()
